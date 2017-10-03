7eaea3771f54858667afcb1e8f27b7e62d94ebec
Zondug Kim <zondugkim@Zonbook-VV.local>
2017-10-03 23:51:41 +0700

    First Commit

--- cardfliptest/ViewController.swift
+++ cardfliptest/ViewController.swift
@@ -10,11 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	var myview = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 200))
+	
+	let a = UIImageView(image: #imageLiteral(resourceName: "cardback"))
+	let b = UIImageView(image: #imageLiteral(resourceName: "club_A"))
+	
+	var showingBack = true
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
-	}
 
+		
+		a.frame = myview.bounds
+		b.frame = myview.bounds
+		
+		myview.addSubview(a)
+		
+		myview.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(tap)))
+		view.addSubview(myview)
+
+	}
+	
+	func tap(gesture: UITapGestureRecognizer) {
+		if (showingBack) {
+			
+			UIView.transition(from: a, to: b, duration: 0.3, options: UIViewAnimationOptions.transitionFlipFromRight, completion: nil)
+			showingBack = false
+		} else {
+			UIView.transition(from: b, to: a, duration: 0.3, options: UIViewAnimationOptions.transitionFlipFromRight, completion: nil)
+			showingBack = true
+		}
+	}
+	
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
