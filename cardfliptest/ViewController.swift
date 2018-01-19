98d6a567e2e76a1a0eea6d9b1e41498963d5c687
Zondug Kim <zondug@me.com>
2018-01-19 18:42:18 +0700

    testing CATransition

--- cardfliptest/ViewController.swift
+++ cardfliptest/ViewController.swift
@@ -11,8 +11,10 @@ import UIKit
 class ViewController: UIViewController {
 
 	// make a view
-	var myview = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 200))
+	var myview = UIView()
 	
+	let transitioningLayer = CATextLayer()
+
 	// and front and back of card view
 	let a = UIImageView(image: #imageLiteral(resourceName: "cardback")) // is back
 	let b = UIImageView(image: #imageLiteral(resourceName: "club_A")) // is front
@@ -23,12 +25,15 @@ class ViewController: UIViewController {
 		super.viewDidLoad()
 
 		// put those 2 cards front and back to a view
-		a.frame = myview.bounds
-		b.frame = myview.bounds
+		myview.frame.origin = CGPoint(x: self.view.center.x - a.frame.width/2, y: self.view.center.y - a.frame.height/2)
+		myview.frame.size = a.bounds.size
 		myview.addSubview(a)
 		
 		myview.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(tap)))
 		view.addSubview(myview)
+		
+//		transitioningLayer.frame = myview.frame
+//		myview.layer.addSublayer(transitioningLayer)
 
 	}
 	
@@ -39,14 +44,30 @@ class ViewController: UIViewController {
 		if (showingBack) {
 			
 			// from back to front
-			UIView.transition(from: a, to: b, duration: 0.3, options: UIViewAnimationOptions.transitionFlipFromRight, completion: nil)
+			UIView.transition(from: a, to: b, duration: 0.3, options: .transitionFlipFromRight, completion: nil)
+//			UIView.transition(from: a, to: b, duration: 0.3, options: .transitionCurlUp, completion: nil)
+//			cardTransition()
 			showingBack = false
 		} else {
 			
 			// from front to back
-			UIView.transition(from: b, to: a, duration: 0.3, options: UIViewAnimationOptions.transitionFlipFromRight, completion: nil)
+			UIView.transition(from: b, to: a, duration: 0.3, options: .transitionFlipFromRight, completion: nil)
+			
+//			cardTransition()
 			showingBack = true
 		}
+	}
+	
+	// How to user CATransition
+	func cardTransition() {
+		let transition = CATransition()
+		transition.duration = 0.7
+		
+		transition.type = "pageCurl"
+		transition.subtype = kCATransitionFromRight
+		
+		transitioningLayer.add(transition, forKey: "transition")
+
 	}
 	
 	override func didReceiveMemoryWarning() {
